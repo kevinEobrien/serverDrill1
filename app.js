8843344c02cf3938c2a3e12bc2741168f1f8ab2b
KEVIN OBRIEN <kevinobrien@KEVINs-MacBook-Air.local>
2017-11-14 14:05:39 -0700

    fixed a silly error

--- app.js
+++ app.js
@@ -1,51 +1,61 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT ||3000
-const cohorts = [
-  {
-    id: 1, "Cohort Name": "17-01-WD-DP", "Chort Code": "g100", students: 28
-  },
-  {
-    id: 2, "Cohort Name": "17-01-DS-GT", "Chort Code": "g105", students: 24
-  },
-  {
-    id: 3, "Cohort Name": "17-02-WD-PX", "Chort Code": "g109", students: 30
-  },
-  {
-    id: 4, "Cohort Name": "17-03-WD-BD", "Chort Code": "g110", students: 29
-  }];
+const port = process.env.PORT || 3000;
+const cohorts = [{
+  id: 1,
+  "Cohort Name": "17-01-WD-DP",
+  "Chort Code": "g100",
+  students: 28
+},
+{
+  id: 2,
+  "Cohort Name": "17-01-DS-GT",
+  "Chort Code": "g105",
+  students: 24
+},
+{
+  id: 3,
+  "Cohort Name": "17-02-WD-PX",
+  "Chort Code": "g109",
+  students: 30
+},
+{
+  id: 4,
+  "Cohort Name": "17-03-WD-BD",
+  "Chort Code": "g110",
+  students: 29
+}
+];
 
 
-  function findById(data, id){
-      for (let i = 0; i < data.length; i++){
-          if (data[i].id == id){
-              return data[i];
-          }
-      }
-      return null;
+function findById(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
+    }
   }
+  return null;
+}
 
-app.get("/", function(request, response){
-console.log("hitting route")
+app.get("/", function(request, response) {
   response.json(cohorts);
-})
+});
 
-app.get("/:id", function(request, response){
+app.get("/:id", function(request, response) {
   var record = findById(cohorts, request.params.id);
-    if (!record){
-        response.status(404)
-        response.json({
-            error: {
-                message: "No record found!"
-            }
-        });
+  if (!record) {
+    response.status(404);
+    response.json({
+      error: {
+        message: "No record found!"
       }
+    });
+  }
   response.json(record);
 });
 
 app.listen(port);
-console.log("Blah blah blah blah blah")
